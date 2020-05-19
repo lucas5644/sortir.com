7a90a42af1a9b1bc6456c60c0bff7514adce565e
MugiwaraNoElder <adrien.sureau2020@campus-eni.fr>
2020-05-19 12:04:48 +0200

    commit formulaire de création de sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +16,63 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionMax')
-            ->add('infosSortie')
-            ->add('organisateur')
-            ->add('lieu')
-            ->add('etat')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de l\'évènement',
+                'attr' => [
+                    'class' => 'nom-evenement',
+                    'placeholder' => 'Nom'
+                ]
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et Heure de début',
+                'attr' => [
+                    'class' => 'date-debut'
+                ]
+            ])
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée de l\'évènement',
+                'attr' => [
+                    'class' => 'duree-evenement'
+                ]
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => 'Date limite d\'inscription',
+                'attr' => [
+                    'class' => 'date-limite-inscription'
+                ]
+            ])
+            ->add('nbInscriptionMax', IntegerType::class, [
+                'label' => 'Nombre d\'inscrits maximum',
+                'attr' => [
+                    'class' => 'nb-inscrits-max'
+                ]
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label' => 'Description de l\'évènement',
+                'attr' => [
+                    'class' => 'description-evenement',
+                    'placeholder' => 'Veuillez décrire votre évènement'
+                ]
+            ] )
+            ->add('organisateur', TextType::class, [
+                'label' => 'Organisateur',
+                'attr' => [
+                    'class' => 'organisateur'
+                ]
+            ] )
+            ->add('lieu', TextType::class, [
+                'label' => 'Lieu de l\'évènement',
+                'attr' => [
+                    'class' => 'lieu-evenement'
+                ]
+
+            ])
+            ->add('etat', TextType::class, [
+                'label' => 'Etat',
+                'attr' => [
+                    'class' => 'etat-evenement'
+                ]
+            ])
         ;
     }
 
