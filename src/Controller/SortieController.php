38bf1032b7208788516b8117180031dcfc3223a3
MugiwaraNoElder <adrien.sureau2020@campus-eni.fr>
2020-05-19 15:58:36 +0200

    commit formulaire de création de sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -6,6 +6,7 @@ use App\Entity\Sortie;
 use App\Form\SortieType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
@@ -13,10 +14,18 @@ class SortieController extends AbstractController
     /**
      * @Route("/sortie/createSortie", name="sortie")
      */
-    public function sortie(EntityManagerInterface $em)
+    public function sortie(EntityManagerInterface $em, Request $request)
     {
         $sortie = new Sortie();
         $sortieForm = $this ->createForm(SortieType::class, $sortie);
+
+        $sortieForm->handleRequest($request);
+        dump($sortie);
+        if ($sortieForm->handleRequest($request) && $sortieForm->isValid()){
+            $em->persist($sortie);
+            $em->flush();
+            $this->addFlash("success", "Votre évènement".$sortie->getNom()." a bien été sauvegardé !");
+        }
 
         return $this->render('sortie/createSortie.html.twig', [
             'sortieForm' => $sortieForm -> createView()
