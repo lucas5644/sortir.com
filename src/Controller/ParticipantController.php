e5bfe967c94af8458a7d02237332239959bc858c
Aethesin <arnal.duncan@gmail.com>
2020-05-19 11:22:54 +0200

    Avancée sur le formulaire d'inscription, bientôt en place :bowtie:

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -6,7 +6,9 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\ParticipantType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParticipantController extends AbstractController
@@ -14,13 +16,23 @@ class ParticipantController extends AbstractController
     /**
      * @Route("/signIn", name="signIn")
      */
-    public function signInForm()
+    public function signInForm(Request $request, EntityManagerInterface $em)
     {
         $utilisateur = new Participant();
-        $productForm = $this->createForm(ParticipantType::class, $utilisateur);
+        $userForm = $this->createForm(ParticipantType::class, $utilisateur);
+
+        $userForm->handleRequest($request);
+        dump($utilisateur);
+        if($userForm->isSubmitted() && $userForm->isValid())
+        {
+            $utilisateur->setAdministrateur(false);
+            $utilisateur->setActif(true);
+            $em->persist($utilisateur);
+            return $this->redirectToRoute('home.html.twig', ['id'=>$utilisateur->getId()]);
+        }
 
         return $this->render("User/signIn.html.twig", [
-           "productForm" => $productForm->createView()
+           "userForm" => $userForm->createView()
         ]);
     }
 }
