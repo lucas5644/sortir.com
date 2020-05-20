5a80264b8cdb7fbde677de4823f2be02d5fa7ba8
Lucas <brientlucas56@gmail.com>
2020-05-20 11:50:15 +0200

    Recherche d'une sortie 1.6

--- src/Form/SearchSortieType.php
+++ src/Form/SearchSortieType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
+use App\Entity\Participant;
 use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +18,12 @@ class SearchSortieType extends AbstractType
         $builder
             ->add('nom', TextType::class, [
                 'label'=>'Le nom de la sortie contient : '
+            ])
+            ->add('organisateur', EntityType::class, [
+                'label' => 'Campus',
+                'choice_label' => 'nom',
+                'class'=>Campus::class,
+                'placeholder'=>'Choisir un campus'
             ])
         ;
     }
