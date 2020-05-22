023c8b5a9ffbaa872072af78a7b24d66c963c98a
Aethesin <arnal.duncan@gmail.com>
2020-05-22 10:05:23 +0200

    Rajouts de truc utile vite fait, j'ai plus d'idée de message aujourd'hui :mask:

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,14 +11,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class MainController extends AbstractController
 {
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index(Request $request, SortieRepository $sortieRepository): Response
     {
+
+        if(is_null($this->security->getUser())){
+            return $this->redirectToRoute('login');
+        }
+
         //nouvelle instance de FindSortie
         $findSortie = new FindSortie();
 
