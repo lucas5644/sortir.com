37152ddd5d6311d7fe4278ebdb5bb6a8d5a31264
Aethesin <arnal.duncan@gmail.com>
2020-05-26 12:19:28 +0200

    Y a des trucs de rajouté, en vue admin en tout cas, désactiver, supprimer un user + annuler sortie.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use App\Form\ImportType;
+use App\Form\ParticipantType;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\Reader\Exception;
+use PhpOffice\PhpSpreadsheet\Reader\Ods;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
+
+class AdminController extends AbstractController
+{
+
+    private $entityManager;
+    private $security;
+
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(){
+        $users = $this->entityManager->getRepository(Participant::class)->findAll();
+        return $this->render("admin/index.html.twig", [
+            "users" => $users
+        ]);
+    }
+
+    /**
+     * @Route("/admin/register", name="register")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return RedirectResponse|Response
+     */
+    public function signInForm(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $utilisateur = new Participant();
+        $userForm = $this->createForm(ParticipantType::class, $utilisateur);
+
+        $userForm->handleRequest($request);
+        dump($utilisateur);
+        if($userForm->isSubmitted() && $userForm->isValid())
+        {
+            $password = $passwordEncoder->encodePassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($password);
+            $utilisateur->setActif(true);
+            $em->persist($utilisateur);
+            $em->flush();
+            $this->addFlash("success", "Vous êtes inscrit!!! Bienvenue ".$utilisateur->getPseudo());
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render("User/register.html.twig", [
+            "userForm" => $userForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/annuler/{id}", name="annuler")
+     * @param $id
+     */
+    public function annulerSortie($id){
+        $sortie = $this->entityManager->getRepository(Sortie::class)->findOneBy(['id' => $id]);
+        $etat = $this->entityManager->getRepository(Etat::class)->findOneBy(['id' => 6]);
+        $sortie->setEtat($etat);
+        $this->entityManager->persist($sortie);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('sortie_detail', ['id' => $sortie->getid()]);
+    }
+
+    /**
+     * @Route("/admin/import", name="import")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return RedirectResponse|Response
+     * @throws Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function importUsers(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em){
+
+        $fichierFrom = $this->createForm(ImportType::class);
+        $fichierFrom->handleRequest($request);
+        if($fichierFrom->isSubmitted() && $fichierFrom->isValid()){
+            $urlPhotoFile = $fichierFrom->get('fichier')->getData();
+            if ($urlPhotoFile) {
+                $originalFilename = pathinfo($urlPhotoFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$urlPhotoFile->guessExtension();
+                try {
+                    $urlPhotoFile->move(
+                        $this->getParameter('import_csv_directory'),
+                        $newFilename
+
+                    );
+                    $this->getData($newFilename, $passwordEncoder, $em);
+                } catch (FileException $e) {
+                    $this->addFlash("danger", "Impossible d'enregistrer l'image!!! ");
+                    return $this->redirectToRoute('import');
+                }
+            }
+        }
+
+        return $this->render("User/import.html.twig", [
+            'fichierFrom' => $fichierFrom->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/desactiver/{id}", name="desactiver")
+     * @param $id
+     * @param Request $request
+     */
+    public function desactiverUser($id, Request $request){
+        $user = $this->entityManager->getRepository(Participant::class)->findOneBy(['id' => $id]);
+        $user->setActif(0);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('admin');
+    }
+
+    /**
+     * @Route("/admin/activer/{id}", name="activer")
+     * @param $id
+     * @param Request $request
+     */
+    public function activerUser($id, Request $request){
+        $user = $this->entityManager->getRepository(Participant::class)->findOneBy(['id' => $id]);
+        $user->setActif(1);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('admin');
+    }
+
+    /**
+     * @Route("/admin/supprimer/{id}", name="supprimer")
+     * @param $id
+     * @param Request $request
+     */
+    public function supprimerUser($id, Request $request){
+        $user = $this->entityManager->getRepository(Participant::class)->findOneBy(['id' => $id]);
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('admin');
+    }
+
+
+    /**
+     * @param String $newFilename
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param EntityManagerInterface $em
+     * @throws Exception Méthode pour retrouver les données du fichier csv.
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    private function getData(String $newFilename, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em){
+        $reader = new Ods();
+        $spreadsheet = $reader->load($this->getParameter('kernel.project_dir') . '/public/uploads/imports/'.$newFilename);
+        for ($i = 2; $i < 10000; $i++){
+            $valueDebut = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(1, $i)->getValue();
+            $utilisateur = new Participant();
+            if (is_null($valueDebut)){
+                $i = 10000;
+            }else{
+                $campus = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(1, $i)->getValue();
+                $pseudo = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(2, $i)->getValue();
+                $nom = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(3, $i)->getValue();
+                $prenom = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(4, $i)->getValue();
+                $telephone = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(5, $i)->getValue();
+                $mail = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(6, $i)->getValue();
+                $password = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(7, $i)->getValue();
+                $administrateur = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(8, $i)->getValue();
+                $actif = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(9, $i)->getValue();
+                $url_photo = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(10, $i)->getValue();
+                $campusById = $this->entityManager->getRepository(Campus::class)->findOneBy(['id' => $campus]);
+                $utilisateur->setCampus($campusById);
+                $utilisateur->setPseudo($pseudo);
+                $utilisateur->setNom($nom);
+                $utilisateur->setPrenom($prenom);
+                $utilisateur->setTelephone($telephone);
+                $utilisateur->setPassword($password);
+                $password = $passwordEncoder->encodePassword($utilisateur, $utilisateur->getPassword());
+                $utilisateur->setPassword($password);
+                $utilisateur->setMail($mail);
+                $utilisateur->setAdministrateur($administrateur);
+                $utilisateur->setActif($actif);
+                $utilisateur->setUrlPhoto($url_photo);
+                $em->persist($utilisateur);
+                $em->flush();
+            }
+        }
+    }
+}
