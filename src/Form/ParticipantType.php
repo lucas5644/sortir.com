f17c64e20fc2a701e479614288079af69b01a86a
Emeline G <emeline.giordano@orange.fr>
2020-05-28 01:34:46 +0200

    affichage mon profil

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -21,38 +21,45 @@ class ParticipantType extends AbstractType
                 'label' => 'Pseudo',
                 'required' => true,
             ])
-                ->add('nom', TextType::class, [
-                    'label' => 'Nom',
-                    'required' => true,
-                ])
-                ->add('prenom', TextType::class, [
-                    'label' => 'Prenom',
-                    'required' => true,
-                ])
-                ->add('telephone', TextType::class, [
-                    'label' => 'Téléphone',
-                    'required' => false
-                ])
-                ->add('mail', EmailType::class, [
-                    'label' => 'Email',
-                    'required' => true,
-                ])
-                ->add('password', RepeatedType::class, [
-                    'type' => PasswordType::class,
-                    'invalid_message' => 'Les mots de passe doivent correspondre.',
-                    'options' => ['attr' => ['class' => 'password-field']],
-                    'required' => true,
-                    'first_options' => ['label' => 'Mot de Passe'],
-                    'second_options' => ['label' => 'Confirmation'],
-                ])
-                ->add('administrateur', CheckboxType::class, [
-                    'label' => 'Administrateur ?',
-                    'required' => false,
-                ])
-                ->add('actif')
-                ->add('urlPhoto')
-                ->add('campus')
-        ;
+            ->add('prenom', TextType::class, [
+                 'label' => 'Prénom : ',
+                 'required' => true,
+            ])
+
+            ->add('nom', TextType::class, [
+                'label' => 'Nom : ',
+                'required' => true,
+            ])
+
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone : ',
+                'required' => false
+            ])
+
+            ->add('mail', EmailType::class, [
+                'label' => 'Email : ',
+                'required' => true,
+            ])
+
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Nouveau mot de passe'],
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'second_options' => ['label' => 'Confirmation'],
+            ])
+
+            ->add('administrateur', CheckboxType::class, [
+                'label' => 'Administrateur ?',
+                'required' => false,
+            ])
+
+            ->add('actif')
+
+            ->add('urlPhoto')
+
+            ->add('campus');
     }
 
     public function configureOptions(OptionsResolver $resolver)
