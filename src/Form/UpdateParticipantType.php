70a438305bf3b47873996ebdeaca1156e69680dd
Aethesin <arnal.duncan@gmail.com>
2020-05-28 19:09:03 +0200

    Erreur pseudo unique affichée en message flash :bowtie:

--- src/Form/UpdateParticipantType.php
+++ src/Form/UpdateParticipantType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -20,6 +21,7 @@ class UpdateParticipantType extends AbstractType
         $builder
             ->add('pseudo', TextType::class, [
                 'label' => 'Pseudo :',
+                'required' => true,
             ])
             ->add('nom', TextType::class, [
                 'label' => 'Nom :',
