4c61e6f25e24518c532c3c71d79833460eeb2e77
Lucas <brientlucas56@gmail.com>
2020-05-28 17:11:47 +0200

    Modif bug

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ParticipantRepository::class)
@@ -23,6 +24,7 @@ class Participant implements UserInterface
     private $role;
 
     /**
+     * @Assert\Unique(message="Ce pseudo existe déjà")
      * @ORM\Column(type="string", length=30, unique=true)
      */
     private $pseudo;
